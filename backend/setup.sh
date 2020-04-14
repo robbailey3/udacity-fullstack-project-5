2c662ee8a11cdced372d87f5ab7c1b7a381f89b1
Rob Bailey <rob.bailey3@gmail.com>
2020-04-14 10:13:02 +0100

    amending shell scripts

--- backend/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# init_venv.sh
-if [ -d "./venv/bin" ];then
-  echo "[info] Ctrl+d to deactivate"
-  bash -c ". ./venv/bin/activate; exec /usr/bin/env bash --rcfile <(echo 'PS1=\"(venv)\${PS1}\"') -i;"
-fi
