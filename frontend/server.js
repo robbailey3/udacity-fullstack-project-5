1bb3d51110d2963e5a6df2a32bb897e3f088552e
Rob Bailey <rob.bailey3@gmail.com>
2020-04-14 09:32:49 +0100

    Updating express server config

--- frontend/server.js
+++ frontend/server.js
@@ -3,27 +3,13 @@ const proxy = require('http-proxy-middleware');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
-// Proxy api request
-// Optional, if you don't want your add CORS support to your Django app
-app.use(
-  '/api',
-  proxy({
-    target: process.env.API_ROOT_URL,
-    changeOrigin: true,
-    ws: true,
-    pathRewrite: {
-      '^/api': ''
-    }
-  })
-);
-
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist/frontend')));
 
 // https://facebook.github.io/create-react-app/docs/deployment#serving-apps-with-client-side-routing
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'dist/frontend/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/frontend/', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on :${PORT}`));
