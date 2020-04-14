08f2b49cac55e1052989bd3f64cf6c657ba1d469
Rob Bailey <rob.bailey3@gmail.com>
2020-04-14 09:47:17 +0100

    Updating env files

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://fsnd-rob-backend.herokuapp.com/',
+  auth0: {
+    url: 'fsnd-capstone.eu',
+    audience: 'FSND-Capstone-API',
+    clientId: 'E4VCDqiXdE5PNRag5E6NA6Mdcria1Qef',
+    callbackURL: 'https://fsnd-rob-frontend.herokuapp.com/'
+  }
 };
